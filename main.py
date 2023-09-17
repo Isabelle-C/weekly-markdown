3278fb89878ddbab8b0bf0f22e579f2ecf8b16ad
Isabelle-C <isabellechen2023@u.northwestern.edu>
2023-09-17 12:26:20 -0500

    Update main

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+#!/usr/bin/env python3
 from weekly_markdown.util import Util
 
 
-util = Util()
-util.run()
+def main() -> None:
+    """
+    TODO
+    """
+    filename = "./src/config.yaml"
+    util = Util(filename)
+
+    util.run()
+
+if __name__ == "__main__":
+    main()

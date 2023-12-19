503107df12647e8da1ef43cdf835a2f1b713551b
Isabelle-C <isabellechen2023@u.northwestern.edu>
2023-12-19 08:19:13 +0800

    remove previous files

--- app.py
+++ app.py
@@ -1,19 +1,16 @@
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, url_for, request, redirect, session, g, flash
 from flask_sqlalchemy import SQLAlchemy
+
 from datetime import datetime
+from dateutil.relativedelta import relativedelta, MO
+
 from sqlalchemy import inspect, extract
+import pytz
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/isabellechen/git-repos/weekly-markdown/tasks.db'
+app.secret_key = 'your_secret_key'
 db = SQLAlchemy(app)
-
-with app.app_context():
-    db.drop_all()
-    db.create_all()
-
-    inspector = inspect(db.engine)
-    tables_in_database = inspector.get_table_names()
-    print(tables_in_database)
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -29,16 +26,50 @@ class Todo(db.Model):
     def __repr__(self):
         return '<Task %r>' % self.id
 
+@app.before_request
+def load_timezone():
+    g.timezone = session.get('timezone', 'UTC') 
+
 @app.route('/update_done/<int:task_id>', methods=['POST'])
 def update_done(task_id):
     task = Todo.query.get_or_404(task_id)
     task.done = request.form.get('done') == 'true'
     if task.done:
-        task.done_timestamp = datetime.utcnow()
+        # Get the current time in UTC
+        utc_now = datetime.utcnow()
+
+        # Specify the timezone you want (for example, 'America/New_York')
+        desired_timezone = pytz.timezone(g.timezone)
+
+        # Convert the UTC time to the desired timezone
+        now = utc_now.replace(tzinfo=pytz.utc).astimezone(desired_timezone)
+        task.done_timestamp = now
     else:
         task.done_timestamp = None
     db.session.commit()
     return '', 204
+
+@app.route('/clear_database', methods=['POST'])
+def clear_database():
+    try:
+        Todo.query.delete()  # delete all rows from Task table
+        db.session.commit()  # commit the changes
+        return redirect('/')
+    except:
+        return 'There was a problem deleting that task'
+
+@app.route('/completed')
+def completed():
+    tasks = Todo.query.filter(Todo.done == True).order_by(Todo.due_date).all()
+    # Get the current time in UTC
+    utc_now = datetime.utcnow()
+
+    # Specify the timezone you want (for example, 'America/New_York')
+    desired_timezone = pytz.timezone(g.timezone)
+
+    # Convert the UTC time to the desired timezone
+    now = utc_now.replace(tzinfo=pytz.utc).astimezone(desired_timezone)
+    return render_template('completed.html', tasks=tasks, now=now)
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
@@ -66,17 +97,42 @@ def index():
     else:
         tasks = Todo.query.filter(Todo.done == False).order_by(Todo.due_date).all()
         unique_tags = db.session.query(Todo.tag).distinct().all()
-        now = datetime.utcnow()
-        task_weeks = {(task.due_date.date().year, task.due_date.isocalendar()[1]) for task in tasks}
-        return render_template('index.html', tasks=tasks, unique_tags=unique_tags, now=now, task_weeks=task_weeks)
 
-from dateutil.relativedelta import relativedelta, MO
+        # Get the current time in UTC
+        utc_now = datetime.utcnow()
+
+        # Specify the timezone you want (for example, 'America/New_York')
+        desired_timezone = pytz.timezone(g.timezone)
+
+        # Convert the UTC time to the desired timezone
+        now = utc_now.replace(tzinfo=pytz.utc).astimezone(desired_timezone)
+
+        task_weeks = {(task.due_date.date().year, task.due_date.isocalendar()[1]) for task in tasks}
+        timezones = pytz.all_timezones
+
+        return render_template('index.html', tasks=tasks, unique_tags=unique_tags, now=now, task_weeks=task_weeks, timezones=timezones)
+    
+@app.route('/set_timezone', methods=['POST'])
+def set_timezone():
+    selected_timezone = request.form.get('timezone')
+    session['timezone'] = selected_timezone  # Store the selected timezone in the session
+    flash('Timezone set successfully')  # Flash a success message
+    return redirect('/')  # Redirect back to the index page
 
 @app.route('/week/<int:year>/<int:week>')
 def week(year, week):
     start = datetime.strptime(f'{year}-W{int(week )}-1', "%Y-W%W-%w").date()
     end = start + relativedelta(days=+7)
-    now = datetime.utcnow()
+
+    # Get the current time in UTC
+    utc_now = datetime.utcnow()
+
+    # Specify the timezone you want (for example, 'America/New_York')
+    desired_timezone = pytz.timezone(g.timezone)
+
+    # Convert the UTC time to the desired timezone
+    now = utc_now.replace(tzinfo=pytz.utc).astimezone(desired_timezone)
+    
     tasks = Todo.query.filter(Todo.due_date.between(start, end)).order_by(Todo.due_date).all()
 
     all_tasks = Todo.query.filter(Todo.done == False).order_by(Todo.due_date).all()
@@ -112,5 +168,6 @@ def update(id):
 
     else:
         return render_template('update.html', task=task)
+
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
